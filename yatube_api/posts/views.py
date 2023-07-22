9b8e9c0239cad858a4abf37820ec643f991b943e
tarki2206 <sasha100500@yahoo.com>
2023-07-21 17:54:45 -0700

    404

--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -2,7 +2,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
-from .serializers import GroupSerializer, PostSerializer, CommentSerializer
+from .serializers import GroupSerializer, \
+    PostSerializer, CommentSerializer
 from .models import Group, Post
 
 
@@ -24,5 +25,3 @@ class PostViewSet(viewsets.ModelViewSet):
 @permission_classes([IsAuthenticated])
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-
-
